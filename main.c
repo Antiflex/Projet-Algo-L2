9e6c0b8dbd810bf1a24d381b21667309237102c7
Antiflex <68072873+Antiflex@users.noreply.github.com>
2022-11-13 15:46:47 +0100

    -compareWordWithCform() : compare une cform avec un mot : renvoie 1 si la cform a les mêmes attributs que le mot, 0 sinon -ajout de nbAttributes dans la structure d'un cform -changement de l'attribut de la personne : maintenant "P3" au lieu de "3" -tests de la fonction compareWordWithCform dans le main

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include "files\files.h"
 
 int main() {
+    time_t t;
+    srand(543848484*time(&t));
     /*
     FILE* fichier = NULL;
     setlocale (LC_CTYPE,"");
@@ -99,21 +101,34 @@ int main() {
                     SEEK_CURR+=1;
                     fseek(fichier, 20, SEEK_fin);
                     break;
-
             }
         }
         fclose(fichier);
     }
     return 0;
-
+    */
     t_model model2 = createRandomModel2();
     printDevModel(model2);
-    */
+    model2.words[2].attributes[0] = "IPre";
+    model2.words[2].attributes[1] = "SG";
+    model2.words[2].attributes[2] = "P3";
+    /*
     str mot1 = (str) calloc(4,sizeof(char));
     //mot1[0]= 'o'; mot1[1]= 'u'; mot1[2]= 'i'; mot1[3]= '\0';
 
     char mot2[] = "non";
     addStrChar(&mot1,'0');
     printf("%s",mot1);
-    //printf("%s", concatWordAttributes(model2.words[4]));
+     */
+    //abaisse	abaisser	Ver:IPre+SG+P1:IPre+SG+P3:SPre+SG+P1:SPre+SG+P3:ImPre+SG+P2
+    cform forme1;
+    forme1.nbAttributes = 5;
+    forme1.word = calloc(8,sizeof(char));
+    forme1.word = "abaisse";
+    forme1.attributes = calloc(5,sizeof(str));
+    str tab[5] = {"IPre SG P1", "IPre SG P3", "SPre SG P1", "SPre SG P3", "ImPre SG P2"};
+    forme1.attributes = tab;
+
+    printf("a trouver :[%s]\n", concatWordAttributes(model2.words[2]));
+    printf("%d",compareWordWithCform(model2.words[2],forme1));
 }
